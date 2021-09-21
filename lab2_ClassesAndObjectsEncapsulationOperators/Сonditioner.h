9e625f17f7f045397a4dede32ea33b0ba2dbe847
Nikita Isyuk <Nikita.Isyuk@outlook.com>
2021-09-21 21:29:45 +0300

    lab2 Exercise 3 complete

--- lab2_ClassesAndObjectsEncapsulationOperators/Сonditioner.h
+++ lab2_ClassesAndObjectsEncapsulationOperators/Сonditioner.h
@@ -1,5 +1,35 @@
 #pragma once
+#include <string>
 class Ñonditioner
 {
+private:
+	double tempSum = 0;
+	int numOfChanges = 0;
+	std::string firm;
+	std::string model;
+	double mass;
+	double temp;
+	bool state; // 1-heating 0-chilling
+	std::string releaseYear;
+public:
+	Ñonditioner();
+	Ñonditioner(std::string firm,
+				std::string model,
+				double mass,
+				double temp,
+				bool state,
+				std::string releaseYaer);
+	void setFirm(std::string firm);
+	void setModel(std::string model);
+	void setMass(double mass);
+	void setTemp(double temp);
+	void setState(bool state);
+	void setReleaseYear(std::string releaseYear);
+	void mode(bool state, double temp);
+	void printState();
+	void serialize();
+	void deserialize();
+	void serialize(const std::string& filename) const;
+	void deserialize(const std::string& filename);
 };
 
